32d37aab5e8f923d39c1e6e3f67b999b88f3b1ec
Sasha <volfym79@gmail.com>
2022-07-29 00:26:19 +0200

    init files

---
+++ main.js
@@ -0,0 +1,19 @@
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+Book.prototype.getInfo = function () {
+    let readString = "";
+    if(this.read) {
+        readString = "already read";
+    }
+    else {
+        readString = "not read yet"
+    }
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${readString}`;
+};
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+console.log(theHobbit.getInfo());
